4ecdf78e39d74041f82bb552efdd9713e3faceaa
Alfredo Santoyo <santoyoalfredo@gmail.com>
2016-07-23 14:30:59 -0400

    Added some requirements files showing current dependencies.

--- product/tests.py
+++ product/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 # Create your tests here.
+class FirefoxTestCase(TestCase):
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.addCleanup(self.browser.quit)
+
+	def testPageTitle(self):
+		self.browser.get('http://127.0.0.1:8000/')
+		self.assertIn('Sentiment |', self.browser.title)
+
+if __name__ == '__main__':
+	unittest.main(verbosity=2)
